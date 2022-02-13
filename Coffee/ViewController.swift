198f2658d36d7600badcac2379235bb4207aa168
Dhruv Weaver <dhruv.weaver@gmail.com>
2022-02-12 22:47:54 -0500

    Added loadView() func Initialized with hello world message

--- Coffee/ViewController.swift
+++ Coffee/ViewController.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var msgLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    override func loadView() {
+        view = UIView()
+        view.backgroundColor = .systemBackground
+        
+        msgLabel = UILabel()
+        msgLabel.translatesAutoresizingMaskIntoConstraints = false
+        msgLabel.textAlignment = .center
+        msgLabel.text = "Hello World!"
+        view.addSubview(msgLabel)
+        
+        NSLayoutConstraint.activate([
+            msgLabel.topAnchor.constraint(equalTo: view.layoutMarginsGuide.centerYAnchor),
+            msgLabel.centerXAnchor.constraint(equalTo: view.layoutMarginsGuide.centerXAnchor),
+
+            // more constraints to be added here!
+        ])
+    }
 
 }
 
