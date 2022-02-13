8f233831fd95379ce9f25dc3c17d42492e325813
Dhruv Weaver <dhruv.weaver@gmail.com>
2022-02-13 00:47:16 -0500

    Display json data. Rearranged file structure

--- Coffee/View/ViewController.swift
+++ Coffee/View/ViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
     var msgLabel: UILabel!
+    var loadingIcon: UIActivityIndicatorView!
     
     var coffeeData = CoffeeData()
 
@@ -18,7 +19,7 @@ class ViewController: UIViewController {
         Task.init {
             await coffeeData.getData()
             coffeeData.parseToObject()
-            msgLabel.text = "Coffee type: \(coffeeData.coffeeTypes[0].getTitle())"
+            msgLabel.text = "Coffee type: \(coffeeData.coffeeTypes[1].getTitle())\nDescription:\n\(coffeeData.coffeeTypes[1].getDescription())"
         }
     }
 
@@ -27,9 +28,12 @@ class ViewController: UIViewController {
         view.backgroundColor = .systemBackground
         
         msgLabel = UILabel()
+        msgLabel.numberOfLines = 6
+        msgLabel.lineBreakMode = .byWordWrapping
         msgLabel.translatesAutoresizingMaskIntoConstraints = false
         msgLabel.textAlignment = .center
-        msgLabel.text = "Placeholder"
+        
+        loadingIcon = UIActivityIndicatorView()
         view.addSubview(msgLabel)
         
         NSLayoutConstraint.activate([
